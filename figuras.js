91fe8b845b0f126f550a51960373610eb47b67bd
pcabreram <pcabreram1234@gmail.com>
2021-07-27 22:10:11 -0400

    Calculando area triangulo isoceles

--- figuras.js
+++ figuras.js
@@ -2,31 +2,26 @@ console.group("Cuadrados");
 const ladoCuadrado = 5;
 console.log(`Los lados del cuadrado miden ${ladoCuadrado}cm^2`);
 
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log(`El perimetro del cuadrado es ${perimetroCuadrado}cm^2`);
+const perimetroCuadrado = (lado) => {
+    return lado * 4;
+};
 
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log(`El area del cuadrao es ${areaCuadrado}cm^2`);
+const areaCuadrado = (lado) => {
+    return lado * lado;
+};
 console.groupEnd();
 
 //Código del triángulo
 console.group("Triángulos");
-const ladroTraigulo1 = 6;
-const ladroTraigulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
 
-console.log(
-    `Los lados del triágulo miden ${ladroTraigulo1} cm,${ladroTraigulo2} cm, ${baseTriangulo}`
-);
+const perimetroTriangulo = (lado1, lado2, base) => {
+    return lado1 + lado2 + base;
+};
 
-console.log(`La altura del triágulo es de ${alturaTriangulo} cm`);
+const areaTriangulo = () => {
+    return (base * altura) / 2;
+};
 
-const perimetroTriangulo = ladroTraigulo1 + ladroTraigulo2 + baseTriangulo;
-console.log(`El perimetro del triágulo es de ${perimetroTriangulo} cm`);
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log(`El área del triágulo es de ${areaTriangulo} cm`);
 console.groupEnd();
 //Código del triángulo
 
@@ -34,23 +29,84 @@ console.groupEnd();
 console.group("Círculos");
 
 //Radio
-const radioCirculo = 4;
-console.log(`El radio del circulo es de ${radioCirculo} cm`);
 
 //Diámetro
-const diametroCirculo = radioCirculo * 2;
-console.log(`El diametro del circulo es de ${diametroCirculo} cm`);
+const diametroCirculo = (radio) => {
+    return radio * 2;
+};
 
 //PI
 const PI = Math.PI;
-console.log(`El valro PI es ${PI}`);
 
 //Circuferencia
-const perimetroCirculo = diametroCirculo * PI;
-console.log(`El perimetro del circulo es de ${perimetroCirculo} cm`);
+const perimetroCirculo = (radio) => {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+};
 
 //Area
-const areacirculo = PI * (radioCirculo * radioCirculo);
-console.log(`El área del circulo es de ${areacirculo} cm`);
+const areaCirculo = (radio) => {
+    return radio * radio;
+};
 
+function calcularAreaCuadrado() {
+    let lado = document.getElementById("ladosCuadrado").value;
+    const area = areaCuadrado(lado);
+    alert(area);
+}
+
+function calcularPerimetroCuadrado() {
+    let lado = document.getElementById("ladosCuadrado").value;
+    const perimetro = perimetroCuadrado(lado);
+    alert(perimetro);
+}
+
+/* const obtenerLongitudTriangulo = () => {
+    let lados = [];
+    lados.push(document.getElementById("lado1").value);
+    lados.push(document.getElementById("lado2").value);
+    lados.push(document.getElementById("lado3").value);
+    return lados;
+};
+
+const verificarLados = () => {
+    lados = obtenerLongitudTriangulo();
+    let ladosIguales;
+    console.log(lados);
+
+    if (lados[0] % lados[1] === 0) {
+        ladosIguales = true;
+    }
+
+    if (lados[1] % lados[2] === 0) {
+        ladosIguales = true;
+    }
+
+    if (lados[2] % lados[0] === 0) {
+        ladosIguales = true;
+    }
+    console.log(ladosIguales);
+}; */
+
+function calcularAreaIso() {
+    let a = 0;
+    let b = 0;
+
+    const lado1 = document.getElementById("lado1").value;
+    const lado2 = document.getElementById("lado2").value;
+    const lado3 = document.getElementById("lado3").value;
+
+    lado1 === lado2 ? ((a = lado1), (b = lado3)) : a;
+    lado2 === lado3 ? ((a = lado2), (b = lado1)) : a;
+    lado3 === lado1 ? ((a = lado3), (b = lado2)) : a;
+
+    if (lado1 === lado2 || lado2 === lado3 || lado3 === lado1) {
+        console.log("Es un triagulo Isoceles");
+        console.log(b);
+        let altura = Math.sqrt(a * a - (b / 2) * (b / 2));
+        console.log(altura);
+    } else {
+        console.log("No es un triagulo Isoceles");
+    }
+}
 console.groupEnd();
