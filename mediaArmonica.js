c05041c01730d5737ffc20f7ea15c5ccf53068b2
pcabreram <pcabreram1234@gmail.com>
2021-08-03 22:53:30 -0400

    Casi terminando la tarea

--- mediaArmonica.js
+++ mediaArmonica.js
@@ -1,8 +1,67 @@
-const verificarElementos = (listaElementos) => {
-    listaElementos.forEach(el => {
-        el === 0 ? alert("Existe un valor") : 1
-    });
+const listaElementos = [];
 
-}
+const verificarElementos = (elemento) => {
+    if (elemento === "" || elemento == 0) {
+        return 0;
+    }
+};
 
-verificarElementos([1, 2, 3, 4, 0, 12])
+const agregarElemento = (el) => {
+    const elAdd = verificarElementos(el);
+    if (elAdd == 0) {
+        alert("El valor ingresado no puede ser nulo");
+        document.getElementById("inputAdd").focus();
+        return;
+    } else {
+        let textAreaActValue = document.getElementById("textAreaResult").value;
+        document.getElementById("textAreaResult").value = `${textAreaActValue} 
+        ${el}...`;
+        listaElementos.push(parseInt(el));
+        document.getElementById("inputAdd").value = "";
+        document.getElementById("inputAdd").focus();
+    }
+};
+
+const calcularMediaArmonica = (lista) => {
+    const nNumeros = lista.length;
+    let valorAcumuladoSuma = 0;
+    for (let i = 0; i < lista.length; i++) {
+        valorAcumuladoSuma = valorAcumuladoSuma + 1 / lista[i];
+    }
+    return nNumeros / valorAcumuladoSuma;
+};
+
+document.getElementById("btnCalculate").addEventListener("click", () => {
+    if (listaElementos.length === 0) {
+        alert("Favor introducir por lo menos dos valores");
+        document.getElementById("inputAdd").focus();
+        return;
+    } else {
+        let resultado = calcularMediaArmonica(listaElementos);
+        document.getElementById("resultInput").value = parseFloat(resultado);
+    }
+});
+
+document.getElementById("btnAddvalue").addEventListener("click", () => {
+    let valor = document.getElementById("inputAdd").value;
+    agregarElemento(valor);
+});
+
+document.getElementById("inputAdd").addEventListener("keydown", () => {
+    soloNumeros();
+});
+
+const soloNumeros = () => {
+    let soloNumeros = new RegExp("[0-9]");
+    let inputs = document.querySelectorAll("input");
+
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener("keypress", (e) => {
+            soloNumeros.exec(e.key);
+            console.log(soloNumeros.test(e.key));
+            soloNumeros.test(e.key) ?
+                (e.returnValue = true) :
+                (e.returnValue = false);
+        });
+    }
+};
